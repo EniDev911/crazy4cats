43f3dad769b1f54ba9ae5c77cbb0286655123238
enidev911 <enidev911@gmail.com>
2023-07-28 23:40:03 -0400

    add assoc and validate in reactions

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,2 +1,13 @@
 class Publication < ApplicationRecord
+    belongs_to :user
+    has_many :reactions
+    has_many :users, through: :reactions
+    
+    validates :kind, acceptance: {
+        accept: %w[like dislike]
+    }
+      
+    def self.kinds
+      %w[like dislike]
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :comments
+  has_many :reactions
+  has_many :publications, through: :reactions
+  enum :role, [:normal_user, :admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
