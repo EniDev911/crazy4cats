9d6dbd618c349b1c39f4f78a2fa482677f411383
enidev911 <enidev911@gmail.com>
2023-07-28 23:46:10 -0400

    add partial _counter to reaction

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :publications
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/reactions', to: 'reactions#user_reaction', as: 'user_reaction'
 
   # Defines the root path route ("/")
   # root "articles#index"
